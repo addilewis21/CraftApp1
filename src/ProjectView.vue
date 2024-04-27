856558191ca1c5bb3e5d0bf66741f4c3275ffbe2
addilewis21 <addilewis21@gmail.com>
2024-04-26 19:12:08 -0600

    Edit mode sort of works

--- src/ProjectView.vue
+++ src/ProjectView.vue
@@ -70,14 +70,18 @@ const editMode = ref(false)
 </script>
 
 <template>
+
+<div class="bg-[#F3D3BE]">
+
   <div class="flex justify-end right-0 p-4">
-  <button @click="editMode = !editMode" class="bg-[#B1BA7D] text-black hover:bg-[#7D842A] hover:text-white font-bold py-4 px-6 rounded mt-4 mx-2 text-2xl">{{ editMode ? 'Save' : 'Edit' }}</button>
+    <button @click="saveProject" class="bg-[#B1BA7D] text-black hover:bg-[#7D842A] hover:text-white font-bold py-4 px-6 rounded mt-4 mx-2 text-2xl">Save</button>
+    <button @click="share" class="bg-[#B1BA7D] text-black hover:bg-[#7D842A] hover:text-white font-bold py-4 px-6 rounded mt-4 mx-2 text-2xl">Share</button>
+    <button @click="editMode = !editMode" class="bg-[#B1BA7D] text-black hover:bg-[#7D842A] hover:text-white font-bold py-4 px-6 rounded mt-4 mx-2 text-2xl">{{ editMode ? 'Save Changes' : 'Edit' }}</button>
   </div>
+
   <div class="m-auto ">
+    <div class="m-auto ">
   <img class="m-auto p-4 w-1/4 " :src="image" alt="Project image" /> 
-  <div v-if="editMode">
-    <input v-model="image.value" type="text" placeholder="Image URL">
-  </div>
   <div class="text-center bg-[#E55439] p-2 text-white w-1/2 m-auto">
     <h3 class="text-3xl font-semibold" v-if="id && !editMode">{{ id }}</h3> 
     <div v-if="editMode">
@@ -89,20 +93,6 @@ const editMode = ref(false)
     </div>
   </div>
 </div>
-
-<div class="bg-[#F3D3BE]">
-
-  <div class="flex justify-end right-0 p-4">
-    <button @click="saveProject" class="bg-[#B1BA7D] text-black hover:bg-[#7D842A] hover:text-white font-bold py-4 px-6 rounded mt-4 mx-2 text-2xl">Save</button>
-    <button @click="share" class="bg-[#B1BA7D] text-black hover:bg-[#7D842A] hover:text-white font-bold py-4 px-6 rounded mt-4 mx-2 text-2xl">Share</button>
-  </div>
-
-  <div class="m-auto ">
-    <img class="m-auto p-4 w-1/4 " :src="image" alt="Project image" /> 
-    <div class="text-center bg-[#E55439] p-2 text-white w-1/2 m-auto">
-      <h3 class="text-3xl font-semibold" v-if="id">{{ id }}</h3> 
-      <p>{{ craftType?.toUpperCase() }}</p> 
-    </div>
   </div>
 
 <div class="flex items-center space-x-2 justify-center my-4 ">
