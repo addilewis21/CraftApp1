28ffb09c7804d9abf5ce395d4642486372946a9b
addilewis21 <addilewis21@gmail.com>
2024-04-29 16:56:41 -0600

    seeign what happens

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,11 @@ export default defineConfig({
   plugins: [vue()],
   base: process.env.NODE_ENV === 'production'
     ? '/CraftApp1/'
-    : '/'
+    : '/',
+  build: {
+    rollupOptions: {
+      input: './index.html',
+      external: ['/CraftApp1/assets/index-CUqjo9gl.js']
+    }
+  }
 })
