83d6c9cf884d63f6eedd2f7e74d01e4f94c80109
addilewis21 <addilewis21@gmail.com>
2024-04-30 09:27:13 -0600

    edited base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './', // base public path when served in production
+  base: '/CraftApp1/', // base public path when served in production
   build: {
     outDir: 'dist', // specify the output directory
     rollupOptions: {
